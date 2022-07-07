1a8227e4578f16c4fb8a53f180f6e5a69d9603d8
Basemera Phiona <bsmrrachel@gmail.com>
2022-07-07 23:23:18 +0300

    Remove unwanted folder

--- scheduling/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from users.models import User
-
-# Create your models here.
-
-# class Shift(models.Model):
-#     worker = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
-#     date_of_shift = models.DateField(auto_created=True, null=False, blank=False)
